e06b8ac586dc1f062bb1f85a1a04b891b29792dc
Cloris <cloristang97@gmail.com>
2018-04-02 10:36:03 -0400

    comments

--- shop.js
+++ shop.js
@@ -2,35 +2,35 @@
 $('#all').on('click', function () {
     $('li').not('#all').removeClass('selected');
     $('#all').toggleClass('selected');
-    $('figure').show(1000);
+    $('figure').show();
 })
 $('#b').on('click', function () {
     $('li').not('#b').removeClass('selected');
     $('#b').toggleClass('selected');
-    $('figure').not('.b').hide(1000);
-    $('.b').show(1000);
+    $('figure').not('.b').hide();
+    $('.b').show();
 })
 $('#c').on('click', function () {
     $('li').not('#c').removeClass('selected');
     $('#c').toggleClass('selected');
-    $('figure').not('.c').hide(1000);
-    $('.c').show(1000);
+    $('figure').not('.c').hide();
+    $('.c').show();
 })
 $('#p').on('click', function () {
     $('li').not('#p').removeClass('selected');
     $('#p').toggleClass('selected');
-    $('figure').not('.p').hide(1000);
-    $('.p').show(1000);
+    $('figure').not('.p').hide();
+    $('.p').show();
 })
 $('#i').on('click', function () {
     $('li').not('#i').removeClass('selected');
     $('#i').toggleClass('selected');
-    $('figure').not('.i').hide(1000);
-    $('.i').show(1000);
+    $('figure').not('.i').hide();
+    $('.i').show();
 })
 $('#g').on('click', function () {
     $('li').not('#g').removeClass('selected');
     $('#g').toggleClass('selected');
-    $('figure').not('.g').hide(1000);
-    $('.g').show(1000);
+    $('figure').not('.g').hide();
+    $('.g').show();
 })
